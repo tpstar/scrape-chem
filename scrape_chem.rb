76911d91f0a9823088f2662144f2294ea9e1a388
tpstar <hanlee12@gmail.com>
2017-07-22 10:32:13 -0500

    lets user pick and enter chemical

--- scrape_chem.rb
+++ scrape_chem.rb
@@ -1,6 +1,7 @@
 require 'mechanize' #ruby library that makes automated web interaction easy.
 
-chem_search_term = "toluene"
+puts "Please enter the name of a chemical."
+chem_search_term = gets.strip
 agent = Mechanize.new
 page = agent.get("http://www.sigmaaldrich.com/catalog/search?term=#{chem_search_term}
   &interface=All&N=0&mode=match%20partialmax&lang=en&region=US&focus=product")
